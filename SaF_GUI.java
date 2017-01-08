ae2a5c6142d785459bfb4077c29e5bbf947b905c
andyw1318 <andyw1318@afsenyc.org>
2017-01-08 00:11:14 -0500

    created GUI, test.java for GUI

--- SaF_GUI.java
+++ SaF_GUI.java
@@ -1,11 +1,17 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JButton;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
 
-public class SaF_GUI extends JFrame{
+
+public class SaF_GUI extends JFrame implements ActionListener{
     
-    JPanel p;
-    JButton b;
+    JPanel p; //panel
+    JButton b; //test button
+ 	SFVisualizer s;
 
     public SaF_GUI()
     {
@@ -13,9 +19,12 @@ public class SaF_GUI extends JFrame{
 
         super("Stop and Frisk GUI");
         
+		s = null;
 		p = new JPanel();
-        b = new JButton(" Test "); 
-  
+        //b = new JButton(" Test "); 
+		b = new JButton(" Test ");  		
+		b.addActionListener(this);	
+
         setSize(500,500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(true);
@@ -26,7 +35,25 @@ public class SaF_GUI extends JFrame{
         setVisible(true);
     }
         
-	public static void main(String[] args) //throws IOException
+	public void actionPerformed(ActionEvent event) 
+	{
+        String cmd = event.getActionCommand().intern();
+        if (cmd == " Test ") 
+		{
+			try{
+				s = new SFVisualizer();
+			} catch (IOException e) {
+				System.out.println("IDK whats wrong");
+			}
+				
+            s.filterByRace("A");
+			s.drawData(StdDraw.BLACK, 2000);
+		}
+
+	}
+	
+
+	public static void main(String[] args) 
 	{
 		new SaF_GUI();
 	}
