e04ee511ba6387933476796f53a0a5bcff5f2a01
andyw1318 <andyw1318@afsenyc.org>
2017-01-08 03:41:17 -0500

    Updated
    <3

--- SaF_GUI.java
+++ SaF_GUI.java
@@ -5,13 +5,16 @@ import java.text.*;
 import java.util.*;
 import javax.swing.*;
 import javax.swing.filechooser.FileFilter;
+import javax.swing.JFrame;
+//import javax.swing.JComboBox;
 
 
 public class SaF_GUI extends JFrame implements ActionListener{
     
     JPanel p; //panel
     JButton b; //test button
- 	SFVisualizer s;
+ 	SFVisualizer s; 
+	JComboBox d; //drop down button
 
     public SaF_GUI()
     {
@@ -19,17 +22,32 @@ public class SaF_GUI extends JFrame implements ActionListener{
 
         super("Stop and Frisk GUI");
         
-		s = null;
+		try{
+			s = new SFVisualizer();
+		} 
+		catch (IOException e) {
+			System.out.println("IDK whats wrong");
+		}
+
 		p = new JPanel();
         //b = new JButton(" Test "); 
 		b = new JButton(" Test ");  		
 		b.addActionListener(this);	
+		
+		String[] testArray = {" a ", " b ", " c "};
+		d = new JComboBox(testArray);
+		d.setSelectedIndex(testArray.length - 1);
+		//d.setEditable(true);
+		//System.out.println(d);
+		d.addActionListener(this);
 
         setSize(500,500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(true);
         
         p.add(b);
+		p.add(d);
+
         add(p);
          
         setVisible(true);
@@ -37,18 +55,49 @@ public class SaF_GUI extends JFrame implements ActionListener{
         
 	public void actionPerformed(ActionEvent event) 
 	{
-        String cmd = event.getActionCommand().intern();
+		String abc = "";
+		String cmd = "";
+		try{
+			abc = (String)((JComboBox)event.getSource()).getSelectedItem();
+		}
+		catch (ClassCastException e){
+       		cmd = event.getActionCommand().intern();
+		}
+
+		
+
+
         if (cmd == " Test ") 
 		{
-			try{
-				s = new SFVisualizer();
-			} catch (IOException e) {
-				System.out.println("IDK whats wrong");
-			}
+			
+			//s.filterByBorough("MANHATTAN");
+			//s.filterByRace("B");	
+            s.drawData(StdDraw.BLUE, 2000);
+			
+			
+			
+		} else if (abc == " a ") 
+		{
+			
+			//s.filterByBorough("MANHATTAN");
+            s.filterByRace("B");
+			
+			
+		} else if (abc == " b ") 
+		{
+			
 				
-            s.filterByRace("A");
-			s.drawData(StdDraw.BLACK, 2000);
-		}
+            s.filterByBorough("MANHATTAN");
+			
+			
+		} else if (abc == " c ") 
+		{
+			
+				
+            s.filterByRace("B");
+			
+			
+		} 
 
 	}
 	
@@ -59,5 +108,4 @@ public class SaF_GUI extends JFrame implements ActionListener{
 	}
 
 }
-
 
